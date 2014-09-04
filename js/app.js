236c6b926adb16cc942b8a9922b97182856ff501
Jesús López <jesus.lopez.gnlez@gmail.com>
2014-09-04 15:38:53 +0200

    test03
    test03

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.routes('pato');
   // put your routes here
 });
 
-App.PatoRoute = Ember.Route.extend({
+App.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
